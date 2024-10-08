be5f2b0c7c5c23d7629dea0878b91889147a1989
Prem E <140575115+premsinbox@users.noreply.github.com>
2024-10-08 08:08:31 +0530

    Add files via upload

---
+++ lib/Controller/image_controller.dart
@@ -0,0 +1,72 @@
+import 'package:imagepicker/Model/image_model.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ImageController {
+  final ImagePicker _picker = ImagePicker();
+  ImageModel imageModel = ImageModel();
+
+  Future<bool> _requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return true;
+    } else {
+      var result = await permission.request();
+      return result.isGranted;
+    }
+  }
+
+  Future<bool> _requestGalleryPermission() async {
+    if (await Permission.storage.isGranted) {
+      return true;
+    }
+    if (await Permission.photos.isGranted) {
+      return true;
+    }
+    if (await Permission.storage.request().isGranted) {
+      return true;
+    }
+    if (await Permission.photos.request().isGranted) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<String?> pickImageFromGallery() async {
+    try {
+      if (await _requestGalleryPermission()) {
+        final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+        if (pickedFile != null) {
+          imageModel.imagePath = pickedFile.path;
+          return null; 
+        } else {
+          return "No image selected";
+        }
+      } else {
+        return "Gallery permission denied";
+      }
+    } catch (e) {
+      return "Error picking image from gallery: $e";
+    }
+  }
+
+  Future<String?> pickImageFromCamera({required bool useFrontCamera}) async {
+    try {
+      if (await _requestPermission(Permission.camera)) {
+        final XFile? pickedFile = await _picker.pickImage(
+          source: ImageSource.camera,
+          preferredCameraDevice: useFrontCamera ? CameraDevice.front : CameraDevice.rear,
+        );
+        if (pickedFile != null) {
+          imageModel.imagePath = pickedFile.path;
+          return null; 
+        } else {
+          return "No image captured";
+        }
+      } else {
+        return "Camera permission denied";
+      }
+    } catch (e) {
+      return "Error capturing image: $e";
+    }
+  }
+}
